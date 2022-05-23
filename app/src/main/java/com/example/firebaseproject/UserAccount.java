14014b13d4a350d76a5071f38dd4b57cf365e64f
HongYouBin <ghddbqls33@gmail.com>
2022-05-24 02:01:05 +0900

    firebase auth

---
+++ app/src/main/java/com/example/firebaseproject/UserAccount.java
@@ -0,0 +1,33 @@
+package com.example.firebaseproject;
+
+public class UserAccount {
+    private String idToken;
+    private String emailId;
+    private String password;
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public UserAccount(){ }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
